dea18c8500d64dd4c423fe5c52fdca433d288dca
Anna Marciniec <annamarciniec24601@gmail.com>
2017-11-09 21:21:48 +0100

    Added thread pool and task to compute iterations in coordinates

--- src/main/java/Mandelbrot.java
+++ src/main/java/Mandelbrot.java
@@ -1,5 +1,8 @@
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 import javax.swing.JFrame;
 
 public class Mandelbrot extends JFrame {
@@ -7,7 +10,6 @@ public class Mandelbrot extends JFrame {
     private final int MAX_ITER = 1570;
     private final double ZOOM = 150;
     private BufferedImage I;
-    private double zx, zy, cX, cY, tmp;
 
     public Mandelbrot() {
         super("Mandelbrot Set");
@@ -15,21 +17,28 @@ public class Mandelbrot extends JFrame {
         setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         I = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
+        List<Future<MandelbrotCoordinates>> mandelbrotCoordinatesList = new ArrayList<>();
+        ExecutorService sequenceExecutor = Executors.newFixedThreadPool(100);
         for (int y = 0; y < getHeight(); y++) {
             for (int x = 0; x < getWidth(); x++) {
-                zx = zy = 0;
-                cX = (x - 400) / ZOOM;
-                cY = (y - 300) / ZOOM;
-                int iter = MAX_ITER;
-                while (zx * zx + zy * zy < 4 && iter > 0) {
-                    tmp = zx * zx - zy * zy + cX;
-                    zy = 2.0 * zx * zy + cY;
-                    zx = tmp;
-                    iter--;
-                }
-                I.setRGB(x, y, iter | (iter << 8));
+                Callable<MandelbrotCoordinates> coordinatesCallable = new MandelbrotIterationCounter(MAX_ITER, ZOOM, x, y);
+                mandelbrotCoordinatesList.add(sequenceExecutor.submit(coordinatesCallable));
             }
         }
+        mandelbrotCoordinatesList.forEach(
+                mandelbrotCoordinatesFuture -> {
+                    try {
+                        MandelbrotCoordinates mandelbrotCoordinates = mandelbrotCoordinatesFuture.get();
+
+                        I.setRGB(mandelbrotCoordinates.getX(), mandelbrotCoordinates.getY(),
+                                mandelbrotCoordinates.getIterations()
+                                | mandelbrotCoordinates.getIterations() << 8);
+                    } catch (InterruptedException | ExecutionException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+
     }
 
     @Override
@@ -37,7 +46,4 @@ public class Mandelbrot extends JFrame {
         g.drawImage(I, 0, 0, this);
     }
 
-    public static void main(String[] args) {
-        new Mandelbrot().setVisible(true);
-    }
 }
